9051dd595055a0b7af027a089d2cf56467f42b4e
Gustavo Cabezal <meisterveda@gmail.com>
2022-04-19 10:38:06 -0400

    Fixed Tailwind not working with storybooks problems was the need to add after the import of global.css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,20 @@
 // .storybook/preview.js
-import "../styles/globals.css";
+import "!style-loader!css-loader!postcss-loader!../styles/globals.css";
+// import * as NextImage from 'next/image';
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+// const OriginalNextImage = NextImage.default;
+
+// Object.defineProperty(NextImage, 'default', {
+//   configurable: true,
+//   value: (props) => <OriginalNextImage {...props} unoptimized />,
+// });
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// }
--- stories/components/Loader.stories.jsx
+++ stories/components/Loader.stories.jsx
@@ -3,17 +3,6 @@ import Loader from "../../components/Loader";
 export default {
     title: "components/Loader",
     component: Loader,
-    argTypes: {
-        variant: {
-            options: ['true', 'false'],
-            control: { type: 'radio' },
-        },
-        },
-};
+}
 
-const Template = (args) => <Loader {...args} />;
-
-export const True = Template.bind({});
-True.args = {
-    variant: true,
-};
+export const Default = () => <Loader show={true} />;
