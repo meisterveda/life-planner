52f7c4072ecb9abcc3d25f4394ba23b0b9933082
Gustavo Cabezal <meisterveda@gmail.com>
2022-04-19 16:18:02 -0400

    Added Sidebar

---
+++ stories/components/Sidebar.stories.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Sidebar from '../../components/Sidebar'
+
+export default {
+    title: 'components/Sidebar',
+    component: Sidebar,
+}
+
+export const Default = () => <Sidebar />
