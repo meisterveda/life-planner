1d8b86bd6404063420708b18a7b95cdd60d5c75b
Gustavo Cabezal <meisterveda@gmail.com>
2022-04-21 16:40:02 -0400

    Working with firebase to add signin and signup

---
+++ lib/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+}
+
+export const app = firebase.initializeApp(firebaseConfig)
